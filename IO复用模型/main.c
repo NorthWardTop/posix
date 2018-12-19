bf682fddd310741b7727462349a23854acf05e73
NorthWardTop <2394783277@qq.com>
2018-12-20 01:38:58 +0800

    创建IO复用模型项目

---
+++ IO复用模型/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <sys/tyoes.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+
+
+typedef struct _Server
+{
+    int maxClt;
+    int currClt;
+    
+}Server;
+
+/*****************************************************************************
+ 函 数 名  : main
+ 功能描述  : 主函数
+ 输入参数  : int argc,char** argv
+ 输出参数  : 无
+ 返 回 值  : int
+ 调用函数  : 
+ 被调函数  : 
+ 
+ 修改历史      :
+  1.日    期   : 2018年12月20日
+    作    者   : my
+    修改内容   : 新生成函数
+
+*****************************************************************************/
+int main( int argc,char** argv )
+{
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
